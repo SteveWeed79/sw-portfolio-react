775578e7963fc603155bd33f1dc478fa2ba43b59
unknown <steveweed1979@gmail.com>
2022-11-03 22:06:58 -0500

    Worked on header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
   return (
     <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <h1 className="m-0">Tech Thoughts</h1>
-        <p className="m-0">Get into the mind of a programmer.</p>
+        <h1 className="m-0">Steve Weed</h1>
+        <p className="m-0">Welcome to my portfolio</p>
       </div>
     </header>
   );
