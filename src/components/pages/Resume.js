7a78ee99fde12f600a9943b3fcd59f7a845be4d9
unknown <steveweed1979@gmail.com>
2022-11-08 00:36:51 -0600

    Working on resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,16 +1,11 @@
 import React from "react";
-import DownloadLink from "react-download-link";
+
 import ResumeDoc from "../../images/resume.docx";
 
 export default function Resume() {
   return (
     <div className="home">
       <h1>My Resume</h1>
-      <DownloadLink
-        label="Resume"
-        filename="resume.docx"
-        exportFile={() => ResumeDoc}
-      />
       <p></p>
     </div>
   );
