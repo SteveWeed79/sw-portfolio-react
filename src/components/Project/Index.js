3a7d79261057c6851e1027f19d67aeadd7192221
SteveWeed79 <steveweed1979@gmail.com>
2022-11-08 18:42:11 -0600

    Working on resume

--- src/components/Project/Index.js
+++ src/components/Project/Index.js
@@ -2,7 +2,9 @@ import React from "react";
 // import linkInImg from "../../images/LI-In-Bug.png";
 
 const Project = () => {
-  return <div className="row justify-center">//BUILD Project space here</div>;
+  return (
+    <div className="row justify-center">{/* BUILD Project space here */}</div>
+  );
 };
 
 export default Project;
